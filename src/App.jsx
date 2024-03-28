4878f908bcbbd9e2a29bfcc80a4bbb680c20b445
Budi Gunawan <budiindra.g@gmail.com>
2024-03-28 14:49:57 +0700

    feat: move chakra provider to main.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Employee from "./pages/Employee";
@@ -7,19 +6,15 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <ChakraProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='employee' element={<Employee />} />
-            <Route path='attendace' element={<Attendance />} />
-            <Route path='login' element={<Login />} />
-            <Route path='/' element={<Navigate to='dashboard' replace />} />
-          </Routes>
-        </BrowserRouter>
-      </ChakraProvider>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='dashboard' element={<Dashboard />} />
+        <Route path='employee' element={<Employee />} />
+        <Route path='attendace' element={<Attendance />} />
+        <Route path='login' element={<Login />} />
+        <Route path='/' element={<Navigate to='dashboard' replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

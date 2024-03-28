6914907efe0e19db4e1412361bb34d842b62a36c
Budi Gunawan <budiindra.g@gmail.com>
2024-03-28 15:33:20 +0700

    feat: add handle login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import {
   Flex,
   Box,
@@ -12,6 +13,16 @@ import {
 } from "@chakra-ui/react";
 
 const Login = () => {
+  const formRef = useRef(null);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log({ email, password }, "login payload");
+  };
+
   return (
     <Flex
       minH={"100vh"}
@@ -32,27 +43,40 @@ const Login = () => {
           boxShadow={"lg"}
           p={8}
         >
-          <Stack spacing={4}>
-            <FormControl id='email'>
-              <FormLabel>Email address</FormLabel>
-              <Input type='email' />
-            </FormControl>
-            <FormControl id='password'>
-              <FormLabel>Password</FormLabel>
-              <Input type='password' />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-              >
-                Sign in
-              </Button>
+          <form onSubmit={handleLogin} ref={formRef}>
+            <Stack spacing={4}>
+              <FormControl id='email'>
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type='email'
+                  name='email'
+                  placeholder='johndoe@mail.com'
+                  required
+                />
+              </FormControl>
+              <FormControl id='password'>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type='password'
+                  name='password'
+                  placeholder='enter your password'
+                  required
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  type='submit'
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  Sign in
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
+          </form>
         </Box>
       </Stack>
     </Flex>
